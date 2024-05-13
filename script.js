3eaf824ec0a9471da7d2142689893e858e2a34f2
tanatut12 <tanatut12@gmail.com>
2024-05-13 15:36:31 +0700

    finish

---
+++ script.js
@@ -0,0 +1,28 @@
+const dayNumber = 5;
+
+switch(true) {
+    case dayNumber === 0:
+        console.log("sunday");
+    break;
+    case dayNumber === 2:
+        console.log("monday");
+    break;
+    case dayNumber === 3:
+        console.log("tuesday");
+    break;
+    case dayNumber === 4:
+        console.log("wednesday");
+    break;
+    case dayNumber === 5:
+        console.log("thursday");
+    break;
+    case dayNumber === 6:
+        console.log("friday");
+    break;
+    case dayNumber === 7:
+        console.log("saturday");
+    break;
+    case dayNumber === 8:
+        console.log("Invalid day number");
+    break;
+}
